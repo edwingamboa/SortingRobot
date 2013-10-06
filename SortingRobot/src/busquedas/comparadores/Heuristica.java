b8f8cad3927c25fc62f5d23bd315a7113c2d4c51
Edwin Gamboa <edwjov92@gmail.com>
2013-10-06 05:10:57 -0500

    Heurstica implementada adecuadamenete

---
+++ SortingRobot/src/busquedas/comparadores/Heuristica.java
@@ -0,0 +1,82 @@
+/******************************************************************************
+                                Sorting Robot
+                                
+Inteligencia Artificial: Proyecto No 1
+* Jesús Alexander Aranda Bueno
+
+Presentado por:
+* Roger Fernandez       -  201310229
+* Edwin Gamboa          -  201310233
+* Francisco Rojas       -  201310273
+* David Zuluaga         -  201310294
+
+Clase: Heuristica
+* Clase en donde se organiza la heuristica a manejar en los algoritmos de 
+* búsqueda Avara y A*.
+******************************************************************************/
+
+package busquedas.comparadores;
+
+import sortingrobot.Estado;
+import sortingrobot.Matriz;
+import sortingrobot.Nodo;
+import sortingrobot.IdObjetos;
+
+public class Heuristica implements IdObjetos{   
+    public void Heuristica(){}
+
+    public int calcularH(Nodo nodo){
+        Estado estado=nodo.getEstado();
+        Matriz matriz=estado.getMatriz();
+        int heuristicaUno = 0, heuristicaDos = 0, heuristicASitioUno = 0, 
+                heuristicASitioDos = 0, cargafaltante = 0;        
+       
+        int[] posicionRobot = matriz.retornarCoordenadaDe(ID_ROBOT);
+        int[] posicionSitioUno = matriz.retornarCoordenadaDe(ID_SITIO_UNO);        
+        int[] posicionSitioDos = matriz.retornarCoordenadaDe(ID_SITIO_DOS);        
+                
+        //Distancia de Manhattan desde el SortingRobot hasta el Sitio Uno
+        if(posicionSitioUno!=null){
+            heuristicASitioUno = heuristicaDePosUnoAPosDos(posicionRobot, posicionSitioUno) + matriz.getPesoObjetoUno();
+            //Distancia de Manhattan desde el Sitio Uno hasta el Sitio Dos
+            if(posicionSitioDos!=null)
+                heuristicASitioDos = heuristicaDePosUnoAPosDos(posicionSitioUno, posicionSitioDos) + matriz.getPesoObjetoDos();
+            heuristicaUno = heuristicASitioUno + heuristicASitioDos;
+        }
+        //Distancia de Manhattan desde el SortingRobot hasta el Sitio Dos
+        if(posicionSitioDos!=null){
+            heuristicASitioDos = heuristicaDePosUnoAPosDos(posicionRobot, posicionSitioDos) + matriz.getPesoObjetoDos();
+            //Distancia de Manhattan desde el Sitio Dos hasta el Sitio Uno
+            if(posicionSitioUno!=null)
+                heuristicASitioUno = heuristicaDePosUnoAPosDos(posicionSitioDos, posicionSitioUno) + matriz.getPesoObjetoUno();
+            heuristicaDos = heuristicASitioUno + heuristicASitioDos;
+        }
+        if((heuristicaUno < heuristicaDos && heuristicaUno > 0) || heuristicaDos <= 0){
+            return heuristicaUno;
+        }
+        else{
+            return heuristicaDos;
+        }
+    }
+    
+    
+    public int heuristicaDePosUnoAPosDos(int[] posUno, int[]posDos){
+        if((posUno[0]<posDos[0])&&(posUno[1]<posDos[1]))
+            return (posDos[0]-posUno[0])+(posDos[1]-posUno[1]);
+        else if((posDos[0]<posUno[0])&&(posDos[1]<posUno[1]))
+            return (posUno[0]-posDos[0])+(posUno[1]-posDos[1]);
+        else if((posDos[0]<posUno[0])&&(posUno[1]<posDos[1]))
+            return (posUno[0]-posDos[0])+(posDos[1]-posUno[1]);
+        else if((posUno[0]<posDos[0])&&(posDos[1]<posUno[1]))
+            return (posDos[0]-posUno[0])+(posUno[1]-posDos[1]);
+        else if((posUno[0]==posDos[0])&&(posUno[1]<posDos[1]))
+            return (posDos[1]-posUno[1]);
+        else if((posUno[0]==posDos[0])&&(posDos[1]<posUno[1]))
+            return (posUno[1]-posDos[1]);            
+        else if((posUno[1]==posDos[1])&&(posUno[0]<posDos[0]))
+            return (posDos[0]-posUno[0]); 
+        else if((posUno[1]==posDos[1])&&(posDos[0]<posUno[0]))
+            return (posUno[0]-posDos[0]); 
+        return 0;
+    }
+}
